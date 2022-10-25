6813bdf169bd842802c6f37d1263a5533db1ecba
Leonardo Vega <leo.vega0506@gmail.com>
2022-10-25 15:17:52 -0500

    Agregar archivos de proyecto.

---
+++ APICore/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICore.Models
+{
+    public class Producto
+    {
+        public int IdProductos { get; set; }
+        public string CodigoBarra { get; set; }
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public string Categoria {get; set;}
+        public decimal Precio { get; set; }
+    }
+}
